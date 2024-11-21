8326798448e5636bfab36fc0300ad3177ce28e08
Thiencc03-07-2006 <cct20062250.@gmail.com>
2024-11-21 23:22:30 +0700

    Update session03-8.c

--- session03-8.c
+++ session03-8.c
@@ -5,6 +5,6 @@ int main(){
 	printf("vui long nhap vao so co bon chu so: ");
 	scanf("%4d",&num);
 	int reverse=num%10*1000+num%100/10*100+num%1000/100*10+num/1000;    
-	printf("tong cac so la: %d",reverse);
+	printf("dao so la: %d",reverse);
 	return 0;
 }
