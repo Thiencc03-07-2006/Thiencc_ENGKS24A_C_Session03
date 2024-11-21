44adb390d02e6734ed9d52120075ae7049db6e05
Thiencc03-07-2006 <cct20062250.@gmail.com>
2024-11-21 23:21:41 +0700

    Update session03-8.c

--- session03-8.c
+++ session03-8.c
@@ -3,7 +3,7 @@
 int main(){
 	int num;
 	printf("vui long nhap vao so co bon chu so: ");
-	scanf("%d",&num);
+	scanf("%4d",&num);
 	int reverse=num%10*1000+num%100/10*100+num%1000/100*10+num/1000;    
 	printf("tong cac so la: %d",reverse);
 	return 0;
