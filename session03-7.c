c8743440be43b5edb41fc22e47cf0e164057fd04
Thiencc03-07-2006 <cct20062250.@gmail.com>
2024-11-21 23:20:44 +0700

    Update session03-7.c

--- session03-7.c
+++ session03-7.c
@@ -3,7 +3,7 @@
 int main(){
 	int num;
 	printf("vui long nhap vao so co bon chu so: ");
-	scanf("%d",&num);
+	scanf("%4d",&num);
 	int sum=num/1000+num%1000/100+num%100/10+num%10;    
 	printf("tong cac so la: %d",sum);
 	return 0;
